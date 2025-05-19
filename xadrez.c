9eab6e89f4fd0e37c6c3745d13498ea6d4f927af
roxturon <joao.jp@live.com>
2025-05-19 22:23:03 +0000

    Implementação de While, Do-While e For

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    // Declaração de variantes para While e Do
+    int torre=0, rainha=0;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    printf("\n");
+    //While para representar movimento da torre, indicando até um valor de 5 repetições.
+    while (torre <5)
+    {
+        printf("Torre: Direita.\n");
+        torre++;
+    }
+    printf("\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    //Do para representar movimento da Rainha, indicando até um valor de 8 repetições.
+    do
+    {
+        rainha++;
+        printf("Rainha: esquerda.\n");
+    } while (rainha<8);
+    printf("\n");
+    
+    //For para representar movimento do Bispo, indicando até um valor de 5 repetições e imprimindo duas direções para cada repetição.
+    for (int bispo = 0; bispo < 5; bispo++)
+    {
+        printf("Bispo: Cima.\n");
+        printf("Bispo: Direita.\n");
+    }
+    
 
     return 0;
 }
